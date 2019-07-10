60fde68d6e5bf3e838b453953779bf419c67963a
Behnam <benyblack@gmail.com>
2019-07-10 15:27:53 +0200

    Add doc to show method in chart

--- lib/tools/chart.ex
+++ lib/tools/chart.ex
@@ -1,5 +1,17 @@
 defmodule ExBacktest.Tools.Chart do
-  def show(symbol, data, dates, oscillator_data \\ [%{}], trade_history \\ %{})
+
+  @doc """
+  show Send data to a python script and show a chart on the browser
+
+  ##Parameters
+    - symbol: Symbol name, for example, "BTCUSDT"
+    - data: List of prices
+    - dates: List of dates
+    - oscillator_data: List of Maps contain oscillator indicators
+    - trade_history: Json string of trade history.
+  """
+  @spec show(String.t(), list(float), list(String.t()), list(map), String.t()) :: :ok
+  def show(symbol, data, dates, oscillator_data \\ [%{}], trade_history \\ "{}")
 
   def show(symbol, data, dates, oscillator_data, trade_history) do
     path = './priv/'
